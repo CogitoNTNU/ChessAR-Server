c0b41a41759bc2ca06d543160110c93d521e0c6e
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-04 17:32:45 +0100

    feat: Add Model Interface

---
+++ src/model/model.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+"""
+This interface is responsible for defining the chess model at use to get the best possible moves.
+"""
+
+
+class Model(ABC):
+    """
+    This interface defines how we retrieve the best possible chess move from a given state on the chess board.
+
+    The model layer is responsible for taking the current state of the chess board and returning the best possible move.
+
+    The model layer must implement the following methods:
+    - `get_best_move()`: Returns the best possible move from the current state of the chess board.
+
+    """
+
+    @abstractmethod
+    def get_best_move(self, state: str) -> object:
+        """
+        Returns the best possible move from the current state of the chess board.
+        """
+        pass
