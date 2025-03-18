58202a1886ff9b04c5ee7e829208376882ec129f
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-18 17:35:10 +0100

    feat: Add Stockfish model

---
+++ src/model/stockfish.py
@@ -0,0 +1,17 @@
+from typing import Any
+from model.model import Model
+
+
+class Stockfish(Model):
+    """
+    This implements the Stockfish chess model to get the best possible moves.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def get_best_move(self, state: Any) -> Any:
+        """
+        Returns the best possible move from the current state of the chess board.
+        """
+        pass
