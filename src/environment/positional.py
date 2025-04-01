01fd5cbd55ecf535a20038207abfed5c45c53e60
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-01 17:28:56 +0200

    feat: Add types/data classes for positional environment

--- src/environment/positional.py
+++ src/environment/positional.py
@@ -2,16 +2,35 @@ from typing import Dict, List
 
 from src.environment.environment import Environment
 
+from pydantic import BaseModel
+
 # This is a type alias for a fen string
 fen = str
 Piece = str
 Chessboard = List[List[Piece]]
 
+class Corners(BaseModel):
+    x: float
+    y: float
+    width: float
+    height: float
+
+class PiecePositions(BaseModel):
+    x: float
+    y: float
+    piece: Piece
+    probability: float
+
+class PositionalParams(BaseModel):
+    """The positional arguments for the environment"""
+    corner_positions: List[Corners]
+    piece_positions: List[PiecePositions]
+
 
 class Positional(Environment):
     """Implements an environment that intakes positional arguments from an image, the corner positions and all the piece positions detected"""
 
-    def to_env(self, repr_state: Dict[str, Dict[str, str]]) -> Chessboard:
+    def to_env(self, repr_state: PositionalParams) -> Chessboard:
         """
         Converts/maps the repr_state (positional arguments) to a 2D array of pieces.
 
