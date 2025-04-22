6e04cad3c39794e504f1137d24a9e5fcdf15bf72
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-22 16:52:43 +0200

    feat: Add final dummy structure to wishbone

--- src/representation/wishbone.py
+++ src/representation/wishbone.py
@@ -1,24 +1,28 @@
 from src.environment.positional import Chessboard, Corners, PiecePositions, Positional, PositionalParams
 from src.representation.representation import Representation
-from typing import Any, List
+from typing import List, Literal
+from PIL import Image
+from src.viewport.snapshot import ViewPortImage
 
 
-class WishBoneRepresentation(Representation):
-    """Intakes a stream of images, using two YOLO models to capture chessboard state and outputs it"""
+class WishBone(Representation):
+    type: Literal["wishbone_environment"] = "wishbone_environment"
+
+    """Intakes an image, using two YOLO models to capture chessboard state and outputs it"""
 
     def __init__(self, environment: Positional) -> None:
         self.environment = environment
 
-    def YOLO_detect_corners(self, input: Any) -> List[Corners]:
+    def YOLO_detect_corners(self, input: ViewPortImage) -> List[Corners]:
         """YOLOV# model to detect corners of the chessboard"""
         pass
 
 
-    def YOLO_detect_pieces(self, input: Any) -> List[PiecePositions]:
+    def YOLO_detect_pieces(self, input: ViewPortImage) -> List[PiecePositions]:
         """YOLOV# model to detect pieces and their positions"""
         pass
 
-    def compute(self, input: Any) -> Chessboard:
+    def compute(self, input: Image.Image) -> Chessboard:
         """
         Takes the input from the viewport and returns the state of the chess board.
 
@@ -28,7 +32,8 @@ class WishBoneRepresentation(Representation):
 
         pieces: List[PiecePositions] = self.YOLO_detect_pieces(input) # Get the pieces from the image
 
-        chessboard = self.environment.to_env(PositionalParams(corner_positions=bord_corners, piece_positions=pieces))
+        positional_params = PositionalParams(corner_positions=bord_corners, piece_positions=pieces)
+
+        chessboard = self.environment.to_env(positional_params)
 
         return chessboard
-
