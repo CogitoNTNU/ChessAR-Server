5c970f0d84f615f3dfb114af95e12386e11a6501
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-22 16:53:08 +0200

    feat: Add snapshot viewport and refactor viewport

---
+++ src/viewport/snapshot.py
@@ -0,0 +1,17 @@
+from typing import Literal
+from PIL import Image
+from src.viewport.viewport import ViewPort
+
+type ViewPortImage = Image.Image
+
+PATH_IMAGES = "./misc/images/"
+
+class SnapShot(ViewPort):
+    type: Literal["snapshot_viewport"] = "snapshot_viewport"
+
+    def get_output(self) -> ViewPortImage:
+        """
+        Returns a random image of a chess board or a specified one.
+        """
+
+        return Image.open(f"{PATH_IMAGES}chessboard.jpg")
