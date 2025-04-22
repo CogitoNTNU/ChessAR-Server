6a067eeed092a3cf661f527a6e80ca3c71a05345
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-22 15:06:25 +0200

    refactor: Update name of GenerateRandomData

--- scripts/data-generator/Assets/DataGenerator.cs
+++ scripts/data-generator/Assets/DataGenerator.cs
@@ -1,13 +1,15 @@
 using System;
 using UnityEngine;
 
-public class GenerateRandomData : MonoBehaviour
+public class DataGenerator : MonoBehaviour
 {
-  public int numPictures = 100;
+  public int numPictures = 1000;
   public int currPicture = 0;
-  public SetRandomPosition pos;
-  public int sleep = 200;
+  public int sleep = 10;
   public int interval = 0;
+
+  public SetRandomPosition pos;
+  public LightingGenerator lightingGenerator;
 
   void Update()
   {
@@ -26,6 +28,7 @@ public class GenerateRandomData : MonoBehaviour
       {
         string screenshotName = "Chess_Image_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + "-" + currPicture + ".png";
         string labelName = "Label_data_" + System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss") + "-" + currPicture + ".txt";
+        lightingGenerator.SetRandomLightingValue();
         pos.UpdatePosition();
         pos.SaveImage(screenshotName);
         pos.SaveLabel(labelName);
